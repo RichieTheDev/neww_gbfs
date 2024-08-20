1c6654b769cade6d52664e50c25e86075f41248c
RichieTheDev <richiemomodu@gmail.com>
2024-08-20 22:48:00 +0100

    new

--- modules/quicksight/main.tf
+++ modules/quicksight/main.tf
@@ -1,23 +1,3 @@
-# Define a QuickSight user in your AWS account
-resource "aws_quicksight_user" "default" {
-  # The username for the QuickSight user, provided via a variable
-  user_name = var.quicksight_user_name
-
-  # The email address associated with the QuickSight user
-  email = var.quicksight_user_email
-
-  # Specifies that the user will authenticate via an IAM role or user
-  identity_type = "IAM"
-
-  # Assign the user the "ADMIN" role, which provides full access to QuickSight resources
-  user_role = "ADMIN"
-
-  # The AWS account ID where the QuickSight user is being created
-  aws_account_id = var.aws_account_id
-
-  # The QuickSight namespace where the user will be created, typically "default"
-  namespace = "default"
-}
 # QuickSight User
 resource "aws_quicksight_user" "default" {
   # The username for the QuickSight user
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,48 @@
+provider "aws" {
+  region = var.aws_region
+}
 
+# Include the S3 module
+module "s3" {
+  source = "../modules/s3"
+}
+
+# Include the IAM Role module
+module "iam_role" {
+  source = "../modules/iam_role"
+}
+
+# Include the Lambda function module
+module "lambda" {
+  source         = "../modules/lambda"
+  iam_role_arn   = module.iam_role.role_arn
+  s3_bucket_name = module.s3.bucket_name
+  sns_topic_arn  = module.sns.topic_arn
+}
+
+# Include the CloudWatch module
+module "cloudwatch" {
+  source               = "../modules/cloudwatch"
+  lambda_function_name = module.lambda.function_name
+  sns_topic_arn        = module.sns.topic_arn
+}
+
+# Include the Athena and Glue module
+module "athena_glue" {
+  source         = "../modules/glue"
+  s3_bucket_name = module.s3.bucket_name
+  glue_db_name   = var.glue_db_name
+}
+
+# Include the QuickSight module
+module "quicksight" {
+  source                = "../modules/quicksight"
+  quicksight_user_name  = var.quicksight_user_name
+  quicksight_user_email = var.quicksight_user_email
+  aws_account_id        = var.aws_account_id
+}
+
+# Include the SNS Topic module
+module "sns" {
+  source = "../modules/sns"
+}
